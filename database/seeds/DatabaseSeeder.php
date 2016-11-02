c43f3d367b016467dc2b651abe7fd6096c470775
Adrison Luz <aluz@reweb.com.br>
2016-11-02 00:17:20 -0200

    Criado migratios de niveis e nvaccess e seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,28 +2,30 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-       $this->call(ConfigsSeeder::class);
-	     $this->call(UsersSeeder::class);
-       //$this->call(PaginasSeeder::class);
-       //$this->call(MenusSeeder::class);
-       //$this->call(MenusItensSeeder::class);
-       //$this->call(FormsSeeder::class);
-       //$this->call(FormsItensSeeder::class);
-       //$this->call(BlocosSeeder::class);
-       //$this->call(BlocosPaginasSeeder::class);
-       //$this->call(CategoriasSeeder::class);
-       //$this->call(PostsSeeder::class);
-       //$this->call(BannersSeeder::class);
-       //$this->call(GaleriasSeeder::class);
-       //$this->call(GaleriasItensSeeder::class);
+    public function run() {
+        $this->call(ConfigsSeeder::class);
+        $this->call(UsersSeeder::class);
+        //$this->call(PaginasSeeder::class);
+        //$this->call(MenusSeeder::class);
+        //$this->call(MenusItensSeeder::class);
+        //$this->call(FormsSeeder::class);
+        //$this->call(FormsItensSeeder::class);
+        //$this->call(BlocosSeeder::class);
+        //$this->call(BlocosPaginasSeeder::class);
+        //$this->call(CategoriasSeeder::class);
+        //$this->call(PostsSeeder::class);
+        //$this->call(BannersSeeder::class);
+        //$this->call(GaleriasSeeder::class);
+        //$this->call(GaleriasItensSeeder::class);
+        $this->call(NiveisSeeder::class);
+        $this->call(NvaccessSeeder::class);
     }
+
 }
