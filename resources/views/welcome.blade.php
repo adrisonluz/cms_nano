12689226f031e8ae4e17e0e35e8760519f3bbe7c
Adrison Luz <aluz@reweb.com.br>
2017-06-12 00:15:33 -0300

    Ajustes redirecionamento login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bem vindo!</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Esta será a páagina inicial de seu site.
                 </div>
             </div>
         </div>
